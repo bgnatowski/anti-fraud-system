e2927ba0344f9c621b23cc6bb790d20a0b993364
bgnatowski <bgnatowski00@gmail.com>
2024-08-07 19:34:55 +0200

    add: openapi simple documentation

--- src/main/java/pl/bgnat/antifraudsystem/security/SecurityFilterChainConfig.java
+++ src/main/java/pl/bgnat/antifraudsystem/security/SecurityFilterChainConfig.java
@@ -16,31 +16,32 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityFilterChainConfig {
-    private final AuthenticationProvider authenticationProvider;
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+	private final AuthenticationProvider authenticationProvider;
+	private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(conf -> conf.authenticationEntryPoint(restAuthenticationEntryPoint))
-                .csrf(csrfConf -> csrfConf.disable())
-                .headers(headersConf -> headersConf.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
-                .authorizeHttpRequests(
-                        auth -> auth
-                                    .requestMatchers(HttpMethod.DELETE, "/api/auth/user/*").hasAuthority("ADMINISTRATOR")
-                                    .requestMatchers(HttpMethod.GET, "/api/auth/users").hasAnyAuthority("ADMINISTRATOR", "SUPPORT")
-                                    .requestMatchers(HttpMethod.POST, "/api/antifraud/transaction", "/api/antifraud/transaction/").hasAnyAuthority("MERCHANT", "SUPPORT", "ADMINISTRATOR")
-                                    .requestMatchers(HttpMethod.PUT, "/api/auth/access", "/api/auth/access/", "/api/auth/role", "/api/auth/role/").hasAuthority("ADMINISTRATOR")
-                                    .requestMatchers(HttpMethod.POST, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/", "api/antifraud/stolencard", "api/antifraud/stolencard/", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/", "/api/antifraud/stolencard", "/api/antifraud/stolencard/").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
-                                    .requestMatchers(HttpMethod.DELETE, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/*", "api/antifraud/stolencard", "api/antifraud/stolencard/*", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/*", "/api/antifraud/stolencard", "/api/antifraud/stolencard/*").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
-                                    .requestMatchers(HttpMethod.GET, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/", "api/antifraud/stolencard", "api/antifraud/stolencard/", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/", "/api/antifraud/stolencard", "/api/antifraud/stolencard/").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
-                                    .requestMatchers("/actuator/shutdown").permitAll()
-                                    .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
-                                    .requestMatchers(HttpMethod.PATCH, "/api/auth/user/**").permitAll()
-                                    .anyRequest().authenticated()
-                )
-                .sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .exceptionHandling(conf -> conf.authenticationEntryPoint(restAuthenticationEntryPoint));
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.httpBasic(conf -> conf.authenticationEntryPoint(restAuthenticationEntryPoint))
+				.csrf(csrfConf -> csrfConf.disable())
+				.headers(headersConf -> headersConf.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
+				.authorizeHttpRequests(
+						auth -> auth
+								.requestMatchers(HttpMethod.DELETE, "/api/auth/user/*").hasAuthority("ADMINISTRATOR")
+								.requestMatchers(HttpMethod.GET, "/api/auth/users").hasAnyAuthority("ADMINISTRATOR", "SUPPORT")
+								.requestMatchers(HttpMethod.POST, "/api/antifraud/transaction", "/api/antifraud/transaction/").hasAnyAuthority("MERCHANT", "SUPPORT", "ADMINISTRATOR")
+								.requestMatchers(HttpMethod.PUT, "/api/auth/access", "/api/auth/access/", "/api/auth/role", "/api/auth/role/").hasAuthority("ADMINISTRATOR")
+								.requestMatchers(HttpMethod.POST, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/", "api/antifraud/stolencard", "api/antifraud/stolencard/", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/", "/api/antifraud/stolencard", "/api/antifraud/stolencard/").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
+								.requestMatchers(HttpMethod.DELETE, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/*", "api/antifraud/stolencard", "api/antifraud/stolencard/*", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/*", "/api/antifraud/stolencard", "/api/antifraud/stolencard/*").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
+								.requestMatchers(HttpMethod.GET, "api/antifraud/suspicious-ip", "api/antifraud/suspicious-ip/", "api/antifraud/stolencard", "api/antifraud/stolencard/", "/api/antifraud/suspicious-ip", "/api/antifraud/suspicious-ip/", "/api/antifraud/stolencard", "/api/antifraud/stolencard/").hasAnyAuthority("SUPPORT", "ADMINISTRATOR")
+								.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-resources/**").permitAll()
+								.requestMatchers("/actuator/shutdown").permitAll()
+								.requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
+								.requestMatchers(HttpMethod.PATCH, "/api/auth/user/**").permitAll()
+								.anyRequest().authenticated()
+				)
+				.sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.exceptionHandling(conf -> conf.authenticationEntryPoint(restAuthenticationEntryPoint));
+		return http.build();
+	}
 }
